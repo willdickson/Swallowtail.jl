0b60d0e69cac1010509a2e9ab2f07e70e28fc919
willdickson <wbd@caltech.edu>
2023-04-14 09:53:09 -0700

    Fixed comments on equilib functions constants.

--- src/lattice_const.jl
+++ src/lattice_const.jl
@@ -1,12 +1,12 @@
 
 struct LatticeConst{T}
-    cs  ::T                         # Speed of sound
-    a1  ::T                         # Equilib function constant 1/cs
-    a2  ::T                         # Equilib function constant 1/(2*cs^4)
-    a3  ::T                         # Equilib function constant 1/(2*cs^2)
+    cs  ::T                         # Lattice speed of sound
+    a1  ::T                         # 1st equilib function constant 1/(cs^2)
+    a2  ::T                         # 2nd equilib function constant 1/(2*cs^4)
+    a3  ::T                         # 3rd equilib function constant 1/(2*cs^2)
     w   ::Array{T}                  # Array of lattice weights
     e   ::Vector{SVector{2, Int}}   # Array of lattice vectors
-    ind ::Array{CartesianIndex{2}}  # Lattice vectors index offsets
+    ind ::Array{CartesianIndex{2}}  # Index offsets associated with lattice vectors
     num ::Int                       # Number of lattice vectors
     function LatticeConst{T}() where {T}
         cs = 1/sqrt(3)
